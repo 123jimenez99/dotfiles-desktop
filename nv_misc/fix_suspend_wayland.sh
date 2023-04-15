d79534dbef01c31574fc654b7ab442a7af13c896
Asier Jiménez <123jimenez99@gmail.com>
2023-04-16 00:27:24 +0200

    Add files

--- nv_misc/fix_suspend_wayland.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/path/to/tmp-nvidia" > /etc/modprobe.d/nvidia-power-management.conf
--- ytfzf/conf.sh
+++
@@ -1,1 +0,0 @@
-video_pref="bestvideo[height<=?1440][fps<=?60]+bestaudio/best"
