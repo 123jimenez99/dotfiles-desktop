fcce8e3cc41d6a426967489ea40155f31769e36b
Asier Jiménez <123jimenez99@gmail.com>
2023-05-20 15:37:40 +0200

    postinstall.sh fixes

--- postinstall.sh
+++ postinstall.sh
@@ -1,56 +1,24 @@
 #!/bin/bash
 
-# Install Chromium with minimal dependencies
-echo "Installing Chromium..."
-sudo apt update
-sudo apt install -y --no-install-recommends chromium-browser
+echo "Installing base programs"
+sudo apt update 
+sudo apt install -y --no-install-recommends chromium kmail telegram-desktop libreoffice-writer kate ansible vagrant
 
-# Install Kmail with minimal dependencies
-echo "Installing Kmail..."
-sudo apt install -y --no-install-recommends kmail
-
-# Install Telegram with minimal dependencies
-echo "Installing Telegram..."
-sudo apt install -y --no-install-recommends telegram-desktop
-
-# Install LibreOffice with minimal dependencies
-echo "Installing LibreOffice..."
-sudo apt install -y --no-install-recommends libreoffice-writer
-
-# Install Kate with minimal dependencies
-echo "Installing Kate..."
-sudo apt install -y --no-install-recommends kate
-
-# Install Docker with minimal dependencies
-echo "Installing Docker..."
-sudo apt install -y --no-install-recommends docker.io
-
-# Install Ansible with minimal dependencies
-echo "Installing Ansible..."
-sudo apt install -y --no-install-recommends ansible
-
-# Install Vagrant with minimal dependencies
-echo "Installing Vagrant..."
-sudo apt install -y --no-install-recommends vagrant
 
 # Install KDE desktop environment and applications
 echo "Installing KDE desktop environment and applications..."
-sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark
+sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark powerdevil
 
 # Install misc programs with minimal dependencies
 echo "Installing misc programs..."
-sudo apt install -y --no-install-recommends mpv git
-
-# Install Wine and Lutris properly
+sudo apt install -y --no-install-recommends mpv git ntfs-3g mangohood mangohood:i386
 
 
 # Install Wine
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key 
-https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ 
-https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
 sudo apt update
 sudo apt install --install-recommends winehq-staging
 
@@ -58,9 +26,8 @@ sudo apt install --install-recommends winehq-staging
 sudo apt install wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 
 libdbus-1-3:i386 libsqlite3-0:i386 libvulkan1 libvulkan1:i386
 
-# Add contrib into /etc/apt/sources.list
-echo "deb http://deb.debian.org/debian/ bookworm contrib" | sudo tee 
-/etc/apt/sources.list
+# Add contrib and non-free into /etc/apt/sources.list
+sudo sed -i 's/main$/main contrib non-free/g' /etc/apt/sources.list
 
 # Install Lutris
 sudo apt update
