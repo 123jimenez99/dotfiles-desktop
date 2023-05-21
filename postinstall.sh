df79744a0d40ffe2b3880c77c745af27f0dd42bd
Asier Jiménez <123jimenez99@gmail.com>
2023-05-21 23:45:24 +0200

    postinstall.sh minor fixes

--- postinstall.sh
+++ postinstall.sh
@@ -4,17 +4,19 @@ echo "Installing base programs"
 sudo apt update 
 sudo apt install -y --no-install-recommends chromium kmail telegram-desktop libreoffice-writer kate ansible vagrant
 
+# Add proper scaling to chromium 
+sudo sh -c 'echo "export CHROMIUM_FLAGS=\"\$CHROMIUM_FLAGS --force-device-scale-factor=1.25\"" >> /etc/chromium.d/default-flags'
 
 # Install KDE desktop environment and applications
 echo "Installing KDE desktop environment and applications..."
-sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark powerdevil
+sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark powerdevil plasma-systemmonitor
+
+# Add contrib and non-free into /etc/apt/sources.list
+sudo sed -i 's/main$/main contrib non-free/g' /etc/apt/sources.list
 
 # Install misc programs with minimal dependencies
 echo "Installing misc programs..."
-sudo apt install -y --no-install-recommends mpv git ntfs-3g mangohood mangohood:i386
-
-# Add proper scaling to chromium 
-sudo sh -c 'echo "export CHROMIUM_FLAGS=\"\$CHROMIUM_FLAGS --force-device-scale-factor=1.25\"" >> /etc/chromium.d/default-flags'
+sudo apt install -y --no-install-recommends mpv git streamlink ntfs-3g gamemode mangohood mangohood:i386
 
 # Install Wine
 sudo dpkg --add-architecture i386
@@ -24,17 +26,13 @@ sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/
 sudo apt update
 sudo apt install --install-recommends winehq-staging
 
-# Install Lutris and its dependencies (Nvidia)
+# Install lutris dependencies (Nvidia)
 sudo apt install wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 
 libdbus-1-3:i386 libsqlite3-0:i386 libvulkan1 libvulkan1:i386
-
-# Add contrib and non-free into /etc/apt/sources.list
-sudo sed -i 's/main$/main contrib non-free/g' /etc/apt/sources.list
 
 # Install Lutris
 sudo apt update
 sudo apt install lutris
-
 
 # Cleanup
 echo "Cleaning up..."
