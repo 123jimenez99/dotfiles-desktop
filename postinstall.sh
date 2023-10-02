989d00cb903e8f583c894a682017267b422e0176
Asier Jiménez <80636515+123jimenez99@users.noreply.github.com>
2023-10-02 16:46:25 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -6,14 +6,11 @@ sudo apt install -y --no-install-recommends firefox-esr telegram-desktop kate an
 
 # Install KDE related apps
 echo "Installing KDE desktop environment and applications..."
-sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark powerdevil plasma-systemmonitor
-
-# Add contrib and non-free into /etc/apt/sources.list
-sudo sed -i 's/main$/main contrib non-free/g' /etc/apt/sources.list
+sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark powerdevil bluedevil plasma-systemmonitor
 
 # Install misc programs with minimal dependencies
 echo "Installing misc programs..."
-sudo apt install -y --no-install-recommends mpv git streamlink ntfs-3g gamemode mangohud mangohud:i386
+sudo apt install -y --no-install-recommends mpv git streamlink ntfs-3g libblockdev-crypto2 gamemode gamemode:i386 mangohud mangohud:i386
 
 # Add 32-bit app support
 sudo dpkg --add-architecture i386
@@ -22,7 +19,7 @@ sudo apt install --install-recommends wine
 
 # Install lutris dependencies (Nvidia)
 sudo apt install wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386
-libdbus-1-3:i386 libsqlite3-0:i386 libvulkan1 libvulkan1:i386 nvidia-driver-libs:i386
+libdbus-1-3:i386 libsqlite3-0:i386 nvidia-vulkan-icd libvulkan1 libvulkan1:i386 nvidia-driver-libs:i386
 
 # Install Lutris
 sudo apt update
