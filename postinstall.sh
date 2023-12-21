9dc6b156285dee3319fa8970207de5342f5ea101
123jimenez99 <123jimenez99@gmail.com>
2023-12-21 22:00:59 +0100

    Added Debian-Gnome branch

--- postinstall.sh
+++ postinstall.sh
@@ -2,30 +2,18 @@
 
 # Install essential apps
 sudo apt update 
-sudo apt install -y --no-install-recommends firefox-esr telegram-desktop kate ansible vagrant
+sudo apt install -y --no-install-recommends firefox-esr telegram-desktop
 
-# Install KDE related apps
-echo "Installing KDE desktop environment and applications..."
-sudo apt install -y --no-install-recommends plasma-desktop kwin-x11 sddm konsole plasma-nm systemsettings dolphin kde-spectacle partitionmanager akregator korganizer plasma-pa kscreen gwenview okular systemsettings plasma-discover plasma-discover-backend-flatpak kwalletmanager ark powerdevil bluedevil plasma-systemmonitor
+# Install GNOME related apps
+echo "Installing GNOME desktop environment and applications..."
+sudo apt install -y --no-install-recommends gnome-shell gdm3 gnome-terminal gnome-control-center nautilus gnome-tweaks gnome-calculator gnome-screenshot gnome-system-monitor gnome-music network-manager-gnome gnome-software flatpak gnome-software-plugin-flatpak gnome-shell-extension-manager gnome-shell-extension-tiling-assistant
 
 # Install misc programs with minimal dependencies
 echo "Installing misc programs..."
-sudo apt install -y --no-install-recommends mpv git streamlink ntfs-3g libblockdev-crypto2 gamemode gamemode:i386 mangohud mangohud:i386
-
-# Add 32-bit app support
-sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt install --install-recommends wine
-
-# Install lutris dependencies (Nvidia)
-sudo apt install wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386
-libdbus-1-3:i386 libsqlite3-0:i386 nvidia-vulkan-icd libvulkan1 libvulkan1:i386 nvidia-driver-libs:i386
-
-# Install Lutris
-sudo apt update
-sudo apt install lutris
+sudo apt install -y --no-install-recommends mpv git streamlink ntfs-3g libblockdev-crypto2 intel-media-va-driver mesa-vulkan-drivers
 
 # Cleanup
 echo "Cleaning up..."
 sudo apt autoremove -y
 sudo apt autoclean
+
